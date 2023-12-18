dc963b802d0414e98ac266d47e6dc0354552cb41
Wwikust <wikust@MacBook-Air-Viktoria.local>
2023-12-18 22:20:19 +0300

    DZ wikust L4

---
+++ DZ wikust L4/DZ wikust L4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ wikust L4
+//
+//  Created by Виктория Смолей on 18.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
